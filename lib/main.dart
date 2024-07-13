1f0e8dcb890cd2ff552c0c8fae9bf880f16e1df1
Chris Jerald Maralit <cjgm0920@gmail.com>
2024-07-13 22:57:54 +0800

    first initial homepage added fonts, widgets for location, weather icon, and date_info

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:final_project_appdev/const.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:weather/weather.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,95 +34,171 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required String title});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _MyWidgetState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyWidgetState extends State<HomePage> {
+  final WeatherFactory _weatherFactory = WeatherFactory(OPENWEATHER_API_KEY);
+  
+  Weather? _weather;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String capitalize(String? text) {
+  if (text == null) return "";
+  return text.toUpperCase();
+}
 
   @override
+  void initState(){
+    super.initState();
+    _weatherFactory.currentWeatherByCityName("Batangas").then((weather) {
+      setState(() {
+        _weather = weather;
+      });
+    });
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_weather == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 29, 3, 45), // Dark blue/purple
+            Color.fromARGB(255, 107, 65, 213), // Lighter purple
+          ],
+        ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      child: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.sizeOf(context).height * 0.1),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            locationHeader(),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.01,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            weatherIcon(),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.025,
+            ),
+            dateTimeInfo(),
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.09,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget locationHeader() {
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        color: Color.fromARGB(255, 255, 255, 255),
+        fontFamily: 'Manrope',
+        fontSize: 40,
+        fontWeight: FontWeight.w700,
+      ),
+    );
+  }
+
+  Widget dateTimeInfo() {
+    DateTime current = _weather!.date!;
+    return Column(
+      children: [
+        Text(
+          DateFormat("EEEE").format(current),
+          style: const TextStyle(
+            color: Color.fromARGB(255, 255, 255, 255),
+            fontFamily: 'Manrope',
+            fontSize: 30,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(
+          height: 4,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("h:mm a").format(current),
+              style: const TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontFamily: 'Manrope',
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Text(
+              "  ${DateFormat("|   M.d.y").format(current)}",
+                style: const TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontFamily: 'Manrope',
+                fontSize: 20,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget weatherIcon() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.20,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                "http://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png",
+              ),
+            ),
+          ),
+        ),
+        Text(capitalize(_weather?.weatherDescription ?? ""),
+              style: const TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+              fontFamily: 'Manrope',
+              fontSize: 15,
+              fontWeight: FontWeight.w300,
+          ),        
+        ),
+      ],
     );
   }
 }
